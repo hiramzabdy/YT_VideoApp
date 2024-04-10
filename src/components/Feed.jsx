2dc292a630ba56633693ef78909d5892de601220
Hiram Zabdy <hiramzabdy@hotmail.com>
2024-04-10 02:06:33 -0600

    Basic layout

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import { Box, Stack, Typography} from "@mui/material"
-import Sidebar from "./Sidebar"
+import { Sidebar, Videos } from "./"
 
 const Feed = () => {
   return (
@@ -12,6 +12,13 @@ const Feed = () => {
       >
         <Sidebar></Sidebar>
         <Typography className="copyright" variant="body2" sx={{mt:1.5, color: "#fff"}}>Copyrights 2024</Typography>
+      </Box>
+
+      <Box p={2} sx={{overflow: "auto", height: "30vh", flex: 2}}>
+        <Typography fontWeight="bold" mb={2} sx={{color: "white"}}>
+          New <span style={{color: "#F31503"}}>Videos</span>
+        </Typography>
+        <Videos videos={[]} />
       </Box>
       
     </Stack>
